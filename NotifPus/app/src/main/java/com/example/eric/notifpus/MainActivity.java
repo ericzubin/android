5d244e9d788f868d906e76d518c4ae54c195d946
Eric <maquina.zubin@gmail.com>
2015-09-30 21:27:38 -0500

    weaw
    Hola

--- NotifPus/app/src/main/java/com/example/eric/notifpus/MainActivity.java
+++ NotifPus/app/src/main/java/com/example/eric/notifpus/MainActivity.java
@@ -19,52 +19,9 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Parse.initialize(this, "G5klpHjAC8xFVfOdKoFiIKusHPKguAl2npqIyp9t", "ZIsavOsIzqsuYex5Jsk5xQOSS29Q3AC228XpqEwO");
-        ParseInstallation.getCurrentInstallation().saveInBackground();
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        //Se crea el usuario aqui
-
-        String usuario="usuario";
-        String pass="pass";
-        String email="email@example.com";
-        ParseUser user = new ParseUser();
-        user.setUsername(usuario);
-        user.setPassword(pass);
-        user.setEmail(email);
-// other fields can be set just like with ParseObject
-        user.put("phone", "650-253-0090");
-
-        user.signUpInBackground(new SignUpCallback() {
-
-            @Override
-            public void done(com.parse.ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                }
-            }
-        });
-
-        ParseInstallation installation = ParseInstallation.getCurrentInstallation();
-        installation.put("Usuarios",ParseUser.getCurrentUser());
-        installation.saveInBackground();
-// Create our Installation query; Buscamos en
-        ParseQuery pushQuery = ParseInstallation.getQuery();
-        pushQuery.whereEqualTo("Usuarios", "IUjqUOpbon");
-
-// Send push notification to query
-        ParsePush push = new ParsePush();
-        push.setQuery(pushQuery); // Set our Installation query
-        push.setMessage("Willie Hayes injured by own pop fly.");
-        push.sendInBackground();
 
 
 
